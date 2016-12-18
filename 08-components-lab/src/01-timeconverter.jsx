d91c1a8e5899c8ff9a944f70ca46115cd98f7ce3
edward0001 <edward.kats@outlook.com>
2016-12-18 02:09:51 +0200

    Solution for: 08-components-lab/01-timeconverter.jsx

--- 08-components-lab/src/01-timeconverter.jsx
+++ 08-components-lab/src/01-timeconverter.jsx
@@ -1,12 +1,31 @@
-/**
- * Lab 01: 
- *
- * Implement a time converter component.
- * Component should show 3 input fields labeled hours, minutes and seconds.
- * Typing a value in either field updates the other two with corresponding values.
- */
-
-import React from 'react';
-
-export default React.createClass({
+var TimeConverter = React.createClass({
+    getInitialState: function() {
+        return {
+            hours : 1,
+            minutes : 60,
+            seconds : 3600
+        };
+    },
+    render: function() {
+        return (
+            <div>
+                <h6>Hours</h6>
+                <input type="text" value={this.state.seconds / 3600} onChange={this.handleChange.bind(this, 'hours')} />
+                <br/>
+                <h6>Minutes</h6>
+                <input type="text" value={this.state.seconds / 60} onChange={this.handleChange.bind(this, 'minutes')} />
+                <br/>
+                <h6>Seconds</h6>
+                <input type="text" value={this.state.seconds} onChange={this.handleChange.bind(this, 'seconds')} />
+                <br/>
+            </div>
+        );
+    },
+    handleChange: function (name, e) {
+        var change = {};
+        change[name] = e.target.value;
+        this.setState(change);
+    }
 });
+
+ReactDOM.render(<TimeConverter/>, document.querySelector('main'));
