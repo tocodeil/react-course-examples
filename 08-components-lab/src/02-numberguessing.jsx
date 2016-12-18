76210ff262af0225cf687f5aff126555de600259
ofekam <ofek314@gmail.com>
2016-12-18 13:12:25 +0200

    Solution for: 08-components-lab/02-numberguessing.jsx

--- 08-components-lab/src/02-numberguessing.jsx
+++ 08-components-lab/src/02-numberguessing.jsx
@@ -11,7 +11,69 @@
  * To make the game interesting, computer should sometimes cheat and print wrong messages.
  */
 
+/**
+ * Created by ofek_am on 18/12/2016.
+ */
 import React from 'react';
 
-export default React.createClass({
-});
+export default class NumberGuessing extends React.Component{
+
+    static propTypes = {
+
+    };
+
+    static defaultProps = {
+
+    };
+
+    cheatMode=false;
+
+    number;
+    constructor(props){
+        super(props);
+        this.onChange = this.onChange.bind(this)
+        this.number=Math.floor(Math.random() * (1000 - 0) + 0);
+        setInterval(()=>{
+            this.cheatMode=!this.cheatMode;
+        },3000);
+    }
+
+    state={
+        guess:'',
+        message:"Guess a number"
+    }
+
+    onChange(e){
+        debugger;
+        this.setState({guess:e.target.value});
+        if(!this.cheatMode){
+            if(e.target.value>this.number){
+                this.setState({message:"Too high"});
+            }
+            else if(e.target.value<this.number){
+                this.setState({message:"Too low"});
+            }
+            else{
+                this.setState({message:"Correct!!"});
+            }
+        }
+        else{
+            if(e.target.value<this.number){
+                this.setState({message:"Too high"});
+            }
+            else if(e.target.value>this.number){
+                this.setState({message:"Too low"});
+            }
+            else{
+                this.setState({message:"Correct!!"});
+            }
+        }
+    }
+
+    render(){
+        return <div>
+            <input type="number" value={this.state.guess}  onChange={this.onChange}/><br></br>
+            <label>{this.state.message}</label>
+        </div>
+    }
+}
