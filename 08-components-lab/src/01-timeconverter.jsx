97225cd1f4bf2b3414eceb0cbaff0175a28e3021
heziba <hezi@branovate.com>
2016-12-18 11:49:51 +0200

    Solution for: 08-components-lab/01-timeconverter.jsx

--- 08-components-lab/src/01-timeconverter.jsx
+++ 08-components-lab/src/01-timeconverter.jsx
@@ -1,5 +1,5 @@
 /**
- * Lab 01: 
+ * Lab 01:
  *
  * Implement a time converter component.
  * Component should show 3 input fields labeled hours, minutes and seconds.
@@ -11,27 +11,35 @@ import React from 'react';
 export default class TimeConverter extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { sec: 0,
-                        min: 0,
-                        hour: 0};
+        this.state = {
+            sec: 0,
+            min: 0,
+            hour: 0
+        };
     }
 
     calcSecond = (evt) => {
-        this.setState({sec: evt.target.value,
-                        min: evt.target.value / 60,
-                        hour: (evt.target.value / 60) / 60})
+        this.setState({
+            sec: evt.target.value,
+            min: evt.target.value / 60,
+            hour: (evt.target.value / 60) / 60
+        })
     }
 
     calcMinute = (evt) => {
-        this.setState({sec: evt.target.value * 60,
+        this.setState({
+            sec: evt.target.value * 60,
             min: evt.target.value,
-            hour: evt.target.value / 60})
+            hour: evt.target.value / 60
+        })
     }
 
     calcHour = (evt) => {
-        this.setState({sec: evt.target.value * 60 * 60,
+        this.setState({
+            sec: evt.target.value * 60 * 60,
             min: evt.target.value * 60,
-            hour: evt.target.value})
+            hour: evt.target.value
+        })
     }
 
     render() {
