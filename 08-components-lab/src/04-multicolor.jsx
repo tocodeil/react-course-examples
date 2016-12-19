ce748af649226ea64cd1e1e6050776e1a5abe8c1
tomerz012 <tomer.zeltzer@gmail.com>
2016-12-19 08:36:20 +0200

    Solution for: 08-components-lab/04-multicolor.jsx

--- 08-components-lab/src/04-multicolor.jsx
+++ 08-components-lab/src/04-multicolor.jsx
@@ -14,8 +14,48 @@
  *
  */
 
-import tinycolor from 'tinycolor2';
-import React from 'react';
 
-export default React.createClass({
-});
+import React from 'react';
+import tinycolor from 'tinycolor2';
+
+export default class MultiColor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "#ff0000",
+            cells: []
+        }
+        this.initCellColors();
+    }
+
+    initCellColors() {
+        for (var i=0; i< 10; i++){
+            this.state.cells[i] = this.state.color;
+        }
+    }
+
+    changeColor = (evt) => {
+        this.setState({color: evt.target.value});
+    }
+
+    render(){
+        return <div>
+            <input type="color"  value={tinycolor(this.state.color)} onChange={this.changeColor} />
+            <br />
+            {this.state.cells.map((val, idx) => (
+                <div style={{backgroundColor : this.getColor(idx), width:"50px", height:"50px", display:"inline-block", paddingRight: "15px"}} />
+            ))}
+
+        </div>
+    }
+
+    getColor(idx) {
+        if (idx > 5){
+            return tinycolor(this.state.color).darken((idx-5) * 10);
+        }else if (idx < 5){
+            return tinycolor(this.state.color).lighten((5-idx+1) * 10);
+        }else{
+            return tinycolor(this.state.color);
+        }
+    }
+}
