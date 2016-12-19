002c3aa6caa7fde8fb34f7a2d0e3a5a396540ddb
tomerz012 <tomer.zeltzer@gmail.com>
2016-12-19 13:03:10 +0200

    Solution for: 13-multiplecomponents-lab/01-redspotter.jsx

--- 13-multiplecomponents-lab/src/01-redspotter.jsx
+++ 13-multiplecomponents-lab/src/01-redspotter.jsx
@@ -9,5 +9,51 @@
 
 import React from 'react';
 
-export default React.createClass({
-});
+export default class RedSpotter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 0,
+            redIndex: 0,
+            cells: []
+        }
+        this.initGame()
+    }
+
+    initGame() {
+        this.state.redIndex = Math.floor(Math.random() * 10 );
+        for (var i=0; i < 10; i++){
+            if (i !== this.state.redIndex)
+                this.state.cells[i] = "grey";
+            else
+                this.state.cells[i] = "red";
+        }
+    }
+
+    render(){
+        return <div>
+            <input type="number" disabled="true" value={this.state.score} />
+            <br />
+            {this.state.cells.map((val, idx) => (
+                <div onClick={this.calcScore.bind(this,idx)} style={{backgroundColor : this.state.cells[idx], width:"50px", height:"50px", display:"inline-block",border:"2px", borderColor:"black"}} tabIndex={idx} />
+            ))}
+            <br/>
+            <button onClick={this.resetGame.bind(this)}>Start new game</button>
+        </div>
+    }
+
+
+    resetGame() {
+        this.setState({score: 0});
+        this.initGame();
+    }
+
+    calcScore(idx){
+        if (idx === this.state.redIndex) {
+            this.setState({score: this.state.score + 10});
+             this.initGame();
+        } else {
+            this.setState({score: this.state.score - 5});
+        }
+    }
+}
