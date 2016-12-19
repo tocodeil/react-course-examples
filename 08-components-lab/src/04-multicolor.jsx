340852723d317ed5f6c4ad8ca75e5ae078b5fe47
heziba <hezi@branovate.com>
2016-12-19 17:02:06 +0200

    Solution for: 08-components-lab/04-multicolor.jsx

--- 08-components-lab/src/04-multicolor.jsx
+++ 08-components-lab/src/04-multicolor.jsx
@@ -14,8 +14,71 @@
  *
  */
 
-import tinycolor from 'tinycolor2';
 import React from 'react';
+import tinycolor from 'tinycolor2';
 
-export default React.createClass({
-});
+export default class MultiColor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: '#FDFFFD',
+            boxes: [
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD"),
+                tinycolor("#FDFFFD")
+            ]
+        }
+    }
+
+    changeColor = (evt) => {
+        const boxes = this.state.boxes;
+        for (var i = 0; i < boxes.length ; i++) {
+            const selectedColor = tinycolor(evt.target.value);
+            if (i == Math.ceil(boxes.length / 2) - 1) {
+                boxes[i] = tinycolor(evt.target.value);
+            }else if (i < boxes.length / 2) {
+                boxes[i] = tinycolor(selectedColor.lighten(40 - i *4).desaturate().toString());
+            } else if (i > boxes.length / 2) {
+                boxes[i] = tinycolor(selectedColor.darken(i*4).desaturate().toString());
+            }
+        }
+        this.setState({
+            color: evt.target.value,
+            boxes: boxes
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>04 - MultiColor</h2>
+                <br/>
+                <input type="color" value={this.state.color} onChange={this.changeColor}/>
+                <br/>
+                <br/>
+                <div style={{display: 'inline-block'}}>
+                    {this.state.boxes.map((val, idx) => (
+                        <Box color={val.toString()} ></Box>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export function Box(props) {
+    return (
+        <div style={{width: 50, height: 50, display: 'inline-block', backgroundColor: props.color}}></div>
+    );
+}
+
+Box.propTypes = {
+    color: React.PropTypes.string
+};
+
