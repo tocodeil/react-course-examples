08f66c4725cc9345ae881317d43cfb50ed99fb21
SquallLeoVIII <ozsaat@gmail.com>
2016-12-25 13:06:52 +0200

    Solution for: 17-lifecycle-lab/01-cps.jsx

--- 17-lifecycle-lab/src/01-cps.jsx
+++ 17-lifecycle-lab/src/01-cps.jsx
@@ -8,11 +8,56 @@
 
 import React from 'react';
 
-export default createClass({
+export default class CPS extends React.Component{
+  constructor(props) {
+      super(props);
+      this.state = {
+        clicks: 0,
+        seconds: 0,
+      };
+  }
+
+  buttonClick = (ev) => {
+      let clicks = this.state.clicks + 1;
+      this.setState({ clicks });
+  };
+
+  message = (ev) => {
+      if (this.state.clicks > this.state.seconds * 2.1) {
+          return "too fast";
+      }
+      else if (this.state.clicks <= this.state.seconds) {
+          return "too slow";
+      } else {
+        return "just fine :)";
+      }
+  }
+
+  tick = (ev) => {
+      let seconds = this.state.seconds + 1;
+      let clicks = this.state.clicks;
+      console.log("time " + seconds);
+      if (seconds > this.state.clicks + 3) {
+        seconds = 0;
+        clicks = 0;
+      }
+      let message = this.message();
+      this.setState({ seconds, clicks, message });
+  }
+
+  componentWillMount() {
+      this.timer = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.timer);
+  }
+
   render() {
     return (<div>
-      <button>Click Fast</button>
-      <p>CPS rate: {0}</p>
+      <button onClick={this.buttonClick}>Click Fast</button>
+      <p>CPS rate: {this.state.clicks > 0 ? this.state.clicks/this.state.seconds : 0}</p>
+      <p>{this.message()}</p>
     </div>);
   }
-});
+};
