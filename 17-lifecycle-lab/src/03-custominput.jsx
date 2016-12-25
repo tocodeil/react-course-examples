00d3bf3bbdf2dce124bd0948df0623a769945bcc
SquallLeoVIII <ozsaat@gmail.com>
2016-12-25 13:07:39 +0200

    Solution for: 17-lifecycle-lab/03-custominput.jsx

--- 17-lifecycle-lab/src/03-custominput.jsx
+++ 17-lifecycle-lab/src/03-custominput.jsx
@@ -6,5 +6,43 @@
  * See lab description on the webpage for a live example
  * how this should work.
  */
+import React from 'react';
 
+export default class CustomInput extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputArray: [0,1,2,3],
+        };
+    }
+
+    componentDidMount = () => {
+        this.refs.in0.focus();
+    };
+
+    changeFocus = (key, eve) => {
+        eve.target.value = eve.target.value.slice(-1);
+        if (this.state.inputArray[key+1] === undefined) {
+            key = -1;
+        }
+        key = key +1;
+        this.refs["in"+key].focus();
+    }
+
+    render() {
+        var style = {
+            height: 50,
+            width: 50,
+            fontSize: 30,
+            fontWeight: "bold",
+        }
+        return (
+            <div>
+                {this.state.inputArray.map((val, idx) => (
+                    <input ref={"in"+val} type="text" style={style} key={val} onChange={this.changeFocus.bind(this, val)}></input>
+                ))}
+
+            </div>);
+    }
+};
 
