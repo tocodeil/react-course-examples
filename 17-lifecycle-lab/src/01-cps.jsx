3c8ffeb319d589be238df09d00190e1cf54a6c18
mhbrMalam <mali_ros@malam.com>
2016-12-25 20:48:34 +0200

    Solution for: 17-lifecycle-lab/01-cps.jsx

--- 17-lifecycle-lab/src/01-cps.jsx
+++ 17-lifecycle-lab/src/01-cps.jsx
@@ -1,18 +1,28 @@
-/**
- * Lab 01: Click per seconds
- *
- * Write a component that counts clicks per second.
- * Component should show a button and some text, and print
- * the click-per-seconds rate.
- */
-
 import React from 'react';
 
-export default createClass({
-  render() {
-    return (<div>
-      <button>Click Fast</button>
-      <p>CPS rate: {0}</p>
-    </div>);
-  }
-});
+export default class ClicksCounter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {counter: 0};
+    }
+
+    componentWillMount() {
+        this.timer = setInterval(() => {
+            this.setState({counter: 0});
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    render() {
+        return (<div>
+            <button onClick={(e) => this.setState({counter: this.state.counter + 1})}>Click Fast</button>
+            <div>yo clicked: {this.state.counter}</div>
+            {this.state.counter < 4 && <p>faster please!</p>}
+            {this.state.counter > 4 && <p>calm down..</p>}
+        </div>);
+    }
+}
