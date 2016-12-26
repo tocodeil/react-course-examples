df6efa456e7910c70afd20ffe8cb85280ee58b4a
tomerz012 <tomer.zeltzer@gmail.com>
2016-12-26 12:23:02 +0200

    Solution for: 22-ajax-lab/01-swapi.jsx

--- 22-ajax-lab/src/01-swapi.jsx
+++ 22-ajax-lab/src/01-swapi.jsx
@@ -10,19 +10,117 @@
  * allowing a user to expand from a Person to a Film she played in,
  * to another Person from that film and so on.
  */
-
 import React from 'react';
+import $ from 'jquery';
 
-const Person = React.createClass({
-});
+const PersonDao = {};
 
-const Film = React.createClass({
-});
+PersonDao.search = function (id) {
+    return $.getJSON(`http://swapi.co/api/people/${id}`);
+}
 
-export default React.createClass({
-  render() {
-    return <div>
-      <Person id={1} />
-    </div>
-  }
-});
+const FilmDao = {};
+
+FilmDao.search = function (id) {
+    return $.getJSON(`http://swapi.co/api/films/${id}`);
+}
+
+export default class Person extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {person: {}, visible:false}
+        if (this.props.init)
+            this.getPersonData(this.props.id);
+    }
+
+    getPersonData()  {
+        const xhr = PersonDao.search(this.props.id);
+        xhr.then((res) => {
+            this.setState({ person: res,
+            visible: true});
+        });
+    }
+
+    render() {
+        const person = this.state.person;
+        const visible = this.state.visible;
+        return (
+            <div>
+        {visible ? (this.renderPersonData(person)) : (<button onClick={this.getPersonData.bind(this)}>Person {this.props.index +1}</button>)}
+            </div>);
+    }
+
+    renderPersonData(person) {
+        return (<div>
+            <p><label>Name</label>{person.name}</p>
+            <p><label>Height</label>{person.height}</p>
+            <p><label>Mass</label>{person.mass}</p>
+            <div><label>Films</label><div>{person.films.map((row,idx) => (<Film index={idx} id={this.extractFilmId(person, idx)}  />))}</div></div>
+        </div>);
+    }
+
+    extractFilmId(person, idx){
+        const film = person.films[idx];
+        return Number(film[film.length - 2])
+    }
+}
+
+Person.propTypes = {
+    name: React.PropTypes.string,
+    height: React.PropTypes.number,
+    mass: React.PropTypes.number,
+    films: React.PropTypes.array,
+    id: React.PropTypes.number.isRequired,
+    init: React.PropTypes.bool
+};
+
+
+class Film extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {film: {}, visible: false};
+    }
+
+    getFilmData()  {
+        const xhr = FilmDao.search(this.props.id);
+        xhr.then((res) => {
+            this.setState({ film: res, visible : true });
+        });
+    }
+    renderFilmData(film) {
+        return (<div>
+            <p><label>Title</label>{film.title}</p>
+            <p><label>Episode Id</label>{film.episode_id}</p>
+            <p><label>Director</label>{film.director}</p>
+            <p><label>Producer</label>{film.producer}</p>
+            <div><label>Characters</label>
+                <div>{film.characters.map((row, idx) => (<Person index={idx} id={this.extractPersonId(film,idx)}/>))}</div>
+            </div>
+        </div>);
+    }
+    render() {
+        const film = this.state.film;
+        const visible = this.state.visible;
+        return (
+            <div>
+                {visible ? (this.renderFilmData(film)) : (<button onClick={this.getFilmData.bind(this)}>Film {this.props.index+1}</button>)}
+            </div>);
+    }
+
+    extractPersonId(film, idx){
+        const person = film.characters[idx];
+        return Number(person[person.length - 2])
+    }
+
+}
+
+Film.propTypes = {
+    title: React.PropTypes.string,
+    episode_id: React.PropTypes.number,
+    director: React.PropTypes.string,
+    producer: React.PropTypes.string,
+    characters: React.PropTypes.array,
+    id: React.PropTypes.number.isRequired,
+    index: React.PropTypes.number,
+    showFilmDetails: React.PropTypes.func,
+};
