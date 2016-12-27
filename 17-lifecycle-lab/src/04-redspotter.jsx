a5be121e32ea0d5cc998467a169d064e8c23b2f7
ameed-react <ameed_fah@malam-payroll.com>
2016-12-27 14:06:52 +0200

    Solution for: 17-lifecycle-lab/04-redspotter.jsx

--- 17-lifecycle-lab/src/04-redspotter.jsx
+++ 17-lifecycle-lab/src/04-redspotter.jsx
@@ -3,46 +3,58 @@
  * When a user doesn't do anything for 2 seconds, she should
  * lose 3 points.
  */
-
-import React from 'react';
-import _ from 'underscore';
+import React from "react";
+import _ from "underscore";
 
 const SQUARES_COUNT = 10;
 const SQUARE_STYLE = {
-  width: '100px',
-  height: '100px',
-  background: 'gray',
-  display: 'inline-block',
+	width: '50px', height: '50px', background: 'gray', display: 'inline-block',
 };
 
 const WINNER_STYLE = Object.assign({}, SQUARE_STYLE, {
-  background: 'red',
+	background: 'red',
 });
 
 export default React.createClass({
-  getInitialState() {
-    return {
-      winner: _.random(SQUARES_COUNT),
-      score: 0,
-    };
-  },
+	getInitialState() {
+		return {
+			winner: _.random(SQUARES_COUNT), score: 0,
+		};
+	},
 
-  shuffle() {
-    this.setState({ 
-      winner: _.random(SQUARES_COUNT),
-      score: this.state.score + 5,
-    });
-  },
+	now() {
+		return new Date().getTime();
+	},
 
-  render() {
-    return (<div>
-      <p>Score: {this.state.score}</p>
-      {_.range(SQUARES_COUNT).map((index) => (
-        <div 
-          onClick={() => (this.state.winner === index) && this.shuffle()}
-          style={index === this.state.winner ? WINNER_STYLE : SQUARE_STYLE} />
+	shuffle() {
+		this.setState({
+			winner: _.random(SQUARES_COUNT), score: this.state.score + 5, lastUpdate: this.now(),
+		});
+	},
 
-      ))}
-    </div>);
-  }
+	componentWillMount() {
+		this.state.lastUpdate = this.now();
+		this.timer = setInterval(() => {
+			const now = this.now();
+			if (now - this.state.lastUpdate > 2000) {
+				this.setState({lastUpdate: now, score: this.state.score - 3});
+			}
+		}, 2000);
+	},
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	},
+
+	render() {
+		return (<div>
+			<p>Score: {this.state.score}</p>
+			{_.range(SQUARES_COUNT).map((index) => (
+				<div
+					onClick={() => (this.state.winner === index) && this.shuffle()}
+					style={index === this.state.winner ? WINNER_STYLE : SQUARE_STYLE} key={index}/>
+
+			))}
+		</div>);
+	}
 });
