52813e07f17522f8465748bd7e98b530e8b00965
tomerz012 <tomer.zeltzer@gmail.com>
2016-12-27 20:32:09 +0200

    Solution for: 22-ajax-lab/02-zoomove.jsx

--- 22-ajax-lab/src/02-zoomove.jsx
+++ 22-ajax-lab/src/02-zoomove.jsx
@@ -8,7 +8,27 @@
  */
 
 import React from 'react';
-import zoomove from 'zoomove';
+import $ from 'jquery';
+import {} from 'zoomove/dist/zoomove.min';
 
-export default React.createClass({
-});
+export default class ReactZooMove extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.refs.zooItem.setAttribute("data-zoo-image",this.props.zoomableImage);
+        $(this.refs.zooItem).ZooMove();
+    }
+
+    render() {
+        return (<div>
+            <div ref="zooItem" className="zoo-item"></div>
+        </div>);
+    }
+};
+
+ReactZooMove.propTypes = {
+    zoomableImage: React.PropTypes.string.isRequired,
+};
+
