3493634f90a8a94192e8857cbfb4c78919c1bcd3
advalicht <35960593+advalicht@users.noreply.github.com>
2018-02-06 22:41:53 +0200

    Solution for: 15-components-lab_v16/02-numberguessing.jsx

--- 15-components-lab_v16/src/02-numberguessing.jsx
+++ 15-components-lab_v16/src/02-numberguessing.jsx
@@ -12,6 +12,47 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default React.createClass({
-});
+export default class NumberGuessing extends React.Component {
+
+    static propTypes = {
+        num: PropTypes.number,
+        rand: PropTypes.number
+      };
+    
+      static defaultProps = {
+        num: Math.floor((Math.random() * 1000) + 1),
+        rand: Math.floor((Math.random() * 5) + 1)
+      };
+
+    constructor(props){
+        super(props);
+        this.state = {message : '', guess : 0};
+    }
+
+    showMessage = (e) => {
+        const val = e.target.value;
+        const cheat = this.props.rand == this.state.guess;
+        if(val > this.props.num){
+            this.setState( oldState => 
+                ({message : (cheat ? 'too low' :'too high') , guess : (cheat ? 0 : oldState.guess + 1)})
+            )
+        }else if( val < this.props.num){
+            this.setState( oldState => 
+                ({message : (cheat ? 'too high' : 'too low'), guess : (cheat ? 0 : oldState.guess + 1)})
+            )
+        }else{
+            this.setState( oldState => ({message : 'Success!'}))
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <input type="text" onChange={this.showMessage}/>
+                <p>{this.state.message}</p>
+            </div>
+            )
+    }
+}
