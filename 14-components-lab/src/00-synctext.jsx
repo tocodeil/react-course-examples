4f340f7525086fd4d528072d5245dbbb02efaacb
ynonp <ynonperek@gmail.com>
2018-02-06 19:24:32 +0200

    add synctext

---
+++ 14-components-lab/src/00-synctext.jsx
@@ -0,0 +1,14 @@
+/**
+ * Synchronised input fields
+ *
+ * Create a React component that shows 5 input fields
+ * and synchronises their input:
+ * Each time the value in one input changes,
+ * all other input fields should show that updated value
+ */
+
+import React from 'react';
+
+class MultiInput extends React.Component {
+}
+
