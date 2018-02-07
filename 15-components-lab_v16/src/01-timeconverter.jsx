f72d8c43ca5c0a84774fab0e09a6b9e1f31b20e2
wmashal <wmashal@gmail.com>
2018-02-07 15:56:24 +0200

    Solution for: 15-components-lab_v16/01-timeconverter.jsx

--- 15-components-lab_v16/src/01-timeconverter.jsx
+++ 15-components-lab_v16/src/01-timeconverter.jsx
@@ -1,12 +1,40 @@
-/**
- * Lab 01: 
- *
- * Implement a time converter component.
- * Component should show 3 input fields labeled hours, minutes and seconds.
- * Typing a value in either field updates the other two with corresponding values.
- */
-
 import React from 'react';
 
-export default React.createClass({
-});
+class TimeConvertor extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { Hours : "", Minutes : "" , Seconds : ""};
+      }
+
+      convert = (e) => {
+        if(e.target.id == 'hours'){
+            this.setState({ Hours: e.target.value  });
+            this.setState({ Minutes: e.target.value * 60 });
+            this.setState({ Seconds: e.target.value * 60 * 60 });
+         }
+
+         if(e.target.id == 'min'){
+            this.setState({ Hours: e.target.value / 60  });
+            this.setState({ Minutes: e.target.value });
+            this.setState({ Seconds: e.target.value * 60  });
+         }
+
+         if(e.target.id == 'sec'){
+            this.setState({ Hours: e.target.value /60 / 60 });
+            this.setState({ Minutes: e.target.value / 60 });
+            this.setState({ Seconds: e.target.value  });
+         }
+      }
+
+      render() {
+        return (<div>
+            Hours <input id="hours" onChange={this.convert} value={this.state.Hours} /> <p></p>
+            Minutes <input id="min" onChange={this.convert} value={this.state.Minutes} />  <p></p>
+            Seconds <input id ="sec" onChange={this.convert} value={this.state.Seconds} />  <p></p>
+          </div>);
+      }
+
+}
+
+export default TimeConvertor;
