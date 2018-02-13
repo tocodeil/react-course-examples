186f9b7ad30d86521397b16572d9ad325835a800
advalicht <35960593+advalicht@users.noreply.github.com>
2018-02-13 13:17:41 +0200

    Solution for: 33-lifecycle-lab_v16/01-cps.jsx

--- 33-lifecycle-lab_v16/src/01-cps.jsx
+++ 33-lifecycle-lab_v16/src/01-cps.jsx
@@ -9,12 +9,35 @@
 import React from 'react';
 
 export default class Cps extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { rate  :0 , count : 0 };
+  }
+  
+  
+  componentWillMount = () => {
+    this.timer = setInterval(() => {
+      this.setState({ rate: this.state.count , count : 0});
+    }, 1000);
+  }
+  
+  componentWillUnmount = () => {
+    clearInterval(this.timer);
+  }
+  
+  count = () => {
+    
+    this.setState(oldState => ({count : oldState.count + 1 }));
+  }
+ 
   render() {
-    return (
-      <div>
-        <button>Click Fast</button>
-        <p>CPS rate: {0}</p>
-      </div>
-    );
+    return (<div>
+        <button onClick={this.count} >Click Fast</button>
+        <p>CPS rate: {this.state.rate}</p>
+        <p>{this.state.rate > 4 ? "not so fast..." : "faster"}</p>
+      </div>);
+    
   }
 }
+
+ReactDOM.render(React.createElement(Cps), document.querySelector('main'));
